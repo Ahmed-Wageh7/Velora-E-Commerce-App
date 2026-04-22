671faa6424d1e1ea57190445561ecc02fa388ffc
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-04-22 12:18:37 +0200

    update project varcel settings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { App } from "./app/app";
+import { inject } from "@vercel/analytics";
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    inject({ mode: "auto" });
+  })
   .catch((err) => console.error(err));
