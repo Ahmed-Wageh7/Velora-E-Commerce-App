f70c482fb5d2521a3138855670165f866aa7a429
A7med-Wageh <gimme.your.number.770@gmail.com>
2026-08-10 14:51:45 +0300

    update auth and dashboard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -2,9 +2,13 @@ import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../services/auth.service";
 
-export const authGuard: CanActivateFn = () => {
+export const authGuard: CanActivateFn = async () => {
   const authService = inject(AuthService);
   const router = inject(Router);
+
+  if (authService.isAuthInitializing()) {
+    await authService.restoreSession();
+  }
 
   if (authService.isAuthenticated()) {
     return true;
